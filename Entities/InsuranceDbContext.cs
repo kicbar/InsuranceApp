23b0abde9a4700b8fd5e0e3d5ae627667b5522dd
kicbar <bartlomiejkicior@gmail.com>
2020-12-27 23:39:13 +0100

    add DbContext - DB Connection

---
+++ Entities/InsuranceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InsuranceApp.Entities
+{
+    public class InsuranceDbContext : DbContext
+    {
+        private string _connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=InsuranceDB;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        public DbSet<Contract> Contracts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
