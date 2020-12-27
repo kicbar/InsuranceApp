4690cc203a4c748e4c0ed2f8b4b74e12f9c1a756
kicbar <bartlomiejkicior@gmail.com>
2020-12-28 00:04:50 +0100

    add file to insert base data

---
+++ load_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO [dbo].[Contracts] VALUES ('Thomas Nowak', 'Life', '2020/12/12');
+INSERT INTO [dbo].[Contracts] VALUES ('Harvey Specter', 'Life', '2020/12/31');
+INSERT INTO [dbo].[Contracts] VALUES ('Gregory House', 'Motor', '2020/01/01');
+INSERT INTO [dbo].[Contracts] VALUES ('Donna Paulsen', 'Travel', '2020/01/01');
+
+select * from [dbo].[Contracts];
