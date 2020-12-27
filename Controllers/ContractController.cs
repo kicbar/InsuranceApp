29efbbb88546e3b848cb5a9d46eb81acab379099
kicbar <bartlomiejkicior@gmail.com>
2020-12-28 00:16:22 +0100

    add ContractController and base get method

---
+++ Controllers/ContractController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InsuranceApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceApp.Controllers
+{
+    [ApiController]
+    [Route("api/Insurance")]
+    public class ContractController : Controller
+    {
+        private readonly InsuranceDbContext _insuranceDbContext;
+
+        public ContractController(InsuranceDbContext insuranceDbContext)
+        {
+            _insuranceDbContext = insuranceDbContext;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var contracts = _insuranceDbContext.Contracts;
+            return Ok(contracts);
+        }
+    }
+}
