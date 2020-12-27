b4b47891849133d48f562d9c1a4fbb1d05f9ab13
kicbar <bartlomiejkicior@gmail.com>
2020-12-27 23:25:11 +0100

    add Contract entities

---
+++ Entities/Contract.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InsuranceApp.Entities
+{
+    public class Contract
+    {
+        public int Id { get; set; }
+        public string InsuredPerson { get; set; }
+        public string InsuranceType { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
