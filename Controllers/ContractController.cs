d21bdd3d9d411b0e13f541e90d3875e3253ed633
kicbar <bartlomiejkicior@gmail.com>
2020-12-28 21:46:14 +0100

    add base Get, Post, Put, Delete method to ContractController

--- Controllers/ContractController.cs
+++ Controllers/ContractController.cs
@@ -25,11 +25,51 @@ namespace InsuranceApp.Controllers
             return Ok(contracts);
         }
 
-        [HttpGet("{contractNr}")]
-        public ActionResult Get(string contractNr)
+        [HttpGet("{contractNum}")]
+        public ActionResult Get(string contractNum)
         {
-            var contract = _insuranceDbContext.Contracts.FirstOrDefault(c => c.ContractNr == contractNr);
+            var contract = _insuranceDbContext.Contracts.FirstOrDefault(c => c.ContractNr == contractNum);
             return Ok(contract);
         }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Contract contractModel)
+        {
+            _insuranceDbContext.Contracts.Add(contractModel);
+            _insuranceDbContext.SaveChanges();
+
+            var key = contractModel.ContractNr.Replace(" ", "-").ToLower();
+
+            return Created("api/insurance/" + key, null);
+        }
+
+        [HttpPut("{contractNum}")]
+        public ActionResult Put(string contractNum, [FromBody] Contract contractModel)
+        {
+            var contract = _insuranceDbContext.Contracts.FirstOrDefault(c => c.ContractNr == contractNum);
+
+            if (contract == null)
+                return NotFound();
+
+            contract.ContractNr = contractModel.ContractNr;
+            contract.InsuredPerson = contractModel.InsuredPerson;
+            contract.InsuranceType = contractModel.InsuranceType;
+            contract.StartDate = contractModel.StartDate;
+
+            _insuranceDbContext.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{contractNum}")]
+        public ActionResult Delete(string contractNum)
+        {
+            var contract = _insuranceDbContext.Contracts.FirstOrDefault(c => c.ContractNr == contractNum);
+            _insuranceDbContext.Contracts.Remove(contract);
+            _insuranceDbContext.SaveChanges();
+
+            return NoContent();
+        }
+
     }
 }
