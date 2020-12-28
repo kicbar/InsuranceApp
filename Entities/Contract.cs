413689e87af56a5a42bc51543a03c92cdb4e2743
kicbar <bartlomiejkicior@gmail.com>
2020-12-28 20:35:23 +0100

    add contractnr field to contract entity

--- Entities/Contract.cs
+++ Entities/Contract.cs
@@ -5,6 +5,7 @@ namespace InsuranceApp.Entities
     public class Contract
     {
         public int Id { get; set; }
+        public string ContractNr { get; set; }
         public string InsuredPerson { get; set; }
         public string InsuranceType { get; set; }
         public DateTime StartDate { get; set; }
