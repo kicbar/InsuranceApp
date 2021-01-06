5f3c6a2a30ed0a96228ba340ede92e74ed110623
kicbar <bartlomiejkicior@gmail.com>
2021-01-06 22:06:59 +0100

    add base person validation

--- Startup.cs
+++ Startup.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using InsuranceApp.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,9 +12,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using InsuranceApp.Data;
 using InsuranceApp.HealthCheck;
 using InsuranceApp.Middleware;
+using InsuranceApp.Entities;
+using InsuranceApp.Validators;
+using InsuranceApp.Models;
 
 namespace InsuranceApp
 {
@@ -31,10 +35,12 @@ namespace InsuranceApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                    .AddFluentValidation();
             services.AddDbContext<InsuranceDbContext>();
             services.AddAutoMapper(this.GetType().Assembly);
             services.AddScoped<InsuranceDbInitializer>();
+            services.AddScoped<IValidator<PersonDto>, PersonValidator>();
             services.AddHealthChecks()
                     .AddCheck<HealthCheckApp>("health_check");
             services.AddScoped<LimitRequestsMiddleware>();
---
+++ Validators/PersonValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using InsuranceApp.Data;
+using InsuranceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsuranceApp.Validators
+{
+    public class PersonValidator : AbstractValidator<PersonDto>
+    {
+        public PersonValidator(InsuranceDbContext insuranceDbContext)
+        {
+            RuleFor(p => p.FirstName).NotEmpty();
+            RuleFor(p => p.FirstName).MinimumLength(3);
+            RuleFor(p => p.FirstName).MaximumLength(40);
+            RuleFor(p => p.LastName).NotEmpty();
+            RuleFor(p => p.LastName).MinimumLength(3);
+            RuleFor(p => p.LastName).MaximumLength(40);
+            RuleFor(p => p.Pesel).NotEmpty();
+            RuleFor(p => p.Pesel).Length(11);
+            RuleFor(p => p.Pesel).Custom((value, context) =>
+            {
+                var peselAlredyExist = insuranceDbContext.Persons.Any(x => x.Pesel == value);
+                if (peselAlredyExist)
+                    context.AddFailure("Pesel", "This pesel already exist.");
+            });
+        }
+
+    }
+}
