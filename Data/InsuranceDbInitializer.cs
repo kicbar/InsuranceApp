c52b40a7c62e7b1c7895b52d6a07580db1348af6
kicbar <bartlomiejkicior@gmail.com>
2021-01-06 15:10:36 +0100

    Add data seeder for Persons

--- Data/InsuranceDbInitializer.cs
+++ Data/InsuranceDbInitializer.cs
@@ -20,6 +20,8 @@ namespace InsuranceApp.Data
             {
                 if (!_insuranceDbContext.Contracts.Any())
                     InsertSampleContractData();
+                if (!_insuranceDbContext.Perosns.Any())
+                    InsertSamplePersonData();
             }
         }
 
@@ -44,7 +46,7 @@ namespace InsuranceApp.Data
                 new Contract
                 {
                     ContractNr = "T0101",
-                    InsuredPerson = "DonnaPaulsen",
+                    InsuredPerson = "Donna Paulsen",
                     InsuranceType = "Travel",
                     StartDate = DateTime.Parse("2020-01-01T00:00:00")
                 },
@@ -58,6 +60,48 @@ namespace InsuranceApp.Data
             };
 
             _insuranceDbContext.Contracts.AddRange(contracts);
+            _insuranceDbContext.SaveChanges();
+        }
+
+        public void InsertSamplePersonData()
+        { 
+            var persons = new List<Person>
+            { 
+                new Person
+                { 
+                    FirstName = "Harvey",
+                    LastName = "Specter",
+                    Pesel = "73112156714",
+                    Nationality = "American",
+                    InsertDate = DateTime.Now
+                },
+                new Person
+                {
+                    FirstName = "Gregory",
+                    LastName = "House",
+                    Pesel = "57011899990",
+                    Nationality = "American",
+                    InsertDate = DateTime.Now
+                },
+                new Person
+                {
+                    FirstName = "Donna",
+                    LastName = "Paulsen",
+                    Pesel = "89121204861",
+                    Nationality = "American",
+                    InsertDate = DateTime.Now
+                },
+                new Person
+                {
+                    FirstName = "Thomas",
+                    LastName = "Nowak",
+                    Pesel = "95031262019",
+                    Nationality = "Polish",
+                    InsertDate = DateTime.Now
+                }
+            };
+
+            _insuranceDbContext.Perosns.AddRange(persons);
             _insuranceDbContext.SaveChanges();
         }
 
